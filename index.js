8e54fe33074a024dd146964de465ef0decab0aac
chandan <chandansamantaray784@gmail.com>
2025-11-27 12:41:31 +0530

    basic js class

---
+++ index.js
@@ -0,0 +1,74 @@
+// syntax
+
+// console.log("hii");
+
+// data type and variable
+
+// programiing - comipler - mashine code
+
+// data  - , integer, decimal, boolean
+// array , object, string
+
+//   stack, heap
+
+//   integer - 4bytes
+
+// let name = "chandan"; //14bytes
+// let name2 = "ram"; //6bytes
+
+// let age = 1000; // 4bytes - 32bits
+
+// let markPercentage = 95.22
+// stack , heap
+
+// let is18 = false
+// let isBig = true
+// let x;
+
+// console.log(typeof x);
+
+// undefined
+// let x = null
+
+// null
+
+// Symbol
+
+// variable - empty container or box in memory
+
+// var, let const
+
+// let student1= "milan"
+// var student2 = "chandan"
+
+// rule to create a variable
+
+// letter + number
+
+// let cha34 = "chandan"
+
+//you cannot use space
+// let my name = "kjdkdfjkj"
+//you canot use -
+// let my-name = "dhdjhkfh"
+
+//you can use _
+// let my_name = "chandan"
+
+// cant use number in first of varibale
+// let 1x = 3
+
+//camel case
+// let myName = "chandan"
+// let yourName = "milan"
+
+// snake case
+// let my_name = "fkfh"
+
+// let the_highest_vale = 200
+
+//   getData  getUserData
+
+// MyProduct
+
+//
