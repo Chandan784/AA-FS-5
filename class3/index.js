7785088de09b9fba72d467d08546f21e22902ecc
chandan <chandansamantaray784@gmail.com>
2025-12-22 17:09:33 +0530

    3 and 4 problem solved

--- class3/index.js
+++ class3/index.js
@@ -61,3 +61,54 @@
 
 // Expected Output:
 // 67
+
+// for (let i = 1; i < 50; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum = sum + i;
+// }
+
+// console.log(sum);
+
+// let num = 6543;
+// let revNumber = 0;
+// console.log(Math.trunc(65 / 10));
+
+// revNumber * 10 + digit
+
+// console.log(Math.round(num / 10));
+
+// 3*1000 + 4*100 + 5*10 + 6*1 =3000 + 400 + 50 + 6  = 3456
+
+// while (num !== 0) {
+//   lastDigit = num % 10;
+//   console.log("last dight", lastDigit);
+
+//   revNumber = revNumber * 10 + lastDigit;
+
+//   console.log("reverse number", revNumber);
+
+//   num = Math.floor(num / 10);
+//   console.log("num", num);
+// }
+
+// console.log(revNumber);
+
+// console.log(num / 10 == 0)
+
+// while (num / 10 == 0) {
+
+//    num =
+
+// }
+
+// let num = 5;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(num + "x" + i + " = " + num * i);
+// }
